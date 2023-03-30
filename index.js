296fd21a3bfd167282628dc1b755ded73b42170d
richard <richard.landaeta@gmail.com>
2023-03-29 21:29:11 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+// Define companyName as a const
+const companyName = 'Scuber';
+
+// Define mostProfitableNeighborhood using let
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Define companyCeo using let
+let companyCeo = 'Susan Smith';
